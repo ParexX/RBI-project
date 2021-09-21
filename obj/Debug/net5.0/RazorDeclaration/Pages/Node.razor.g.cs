e2e0a2e5bccf0ca4d351389e39694fdced977ce1
ANTHONYDMN <anthony.domeon@rbi-informatique.fr>
2021-09-21 17:07:54 +0200

    prepare to push

--- obj/Debug/net5.0/RazorDeclaration/Pages/Node.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Node.razor.g.cs
@@ -12,98 +12,98 @@ namespace BlazorSupervisionRBI.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "c:\Users\ADOMEON\blazorsupervisionrbi\_Imports.razor"
+#line 1 "c:\Users\ADOMEON\BlazorSupervisionRBI\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "c:\Users\ADOMEON\blazorsupervisionrbi\_Imports.razor"
+#line 2 "c:\Users\ADOMEON\BlazorSupervisionRBI\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "c:\Users\ADOMEON\blazorsupervisionrbi\_Imports.razor"
+#line 3 "c:\Users\ADOMEON\BlazorSupervisionRBI\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "c:\Users\ADOMEON\blazorsupervisionrbi\_Imports.razor"
+#line 4 "c:\Users\ADOMEON\BlazorSupervisionRBI\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "c:\Users\ADOMEON\blazorsupervisionrbi\_Imports.razor"
+#line 5 "c:\Users\ADOMEON\BlazorSupervisionRBI\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "c:\Users\ADOMEON\blazorsupervisionrbi\_Imports.razor"
+#line 6 "c:\Users\ADOMEON\BlazorSupervisionRBI\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "c:\Users\ADOMEON\blazorsupervisionrbi\_Imports.razor"
+#line 7 "c:\Users\ADOMEON\BlazorSupervisionRBI\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "c:\Users\ADOMEON\blazorsupervisionrbi\_Imports.razor"
+#line 8 "c:\Users\ADOMEON\BlazorSupervisionRBI\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "c:\Users\ADOMEON\blazorsupervisionrbi\_Imports.razor"
+#line 9 "c:\Users\ADOMEON\BlazorSupervisionRBI\_Imports.razor"
 using BlazorSupervisionRBI;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "c:\Users\ADOMEON\blazorsupervisionrbi\_Imports.razor"
+#line 10 "c:\Users\ADOMEON\BlazorSupervisionRBI\_Imports.razor"
 using BlazorSupervisionRBI.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "c:\Users\ADOMEON\blazorsupervisionrbi\_Imports.razor"
+#line 11 "c:\Users\ADOMEON\BlazorSupervisionRBI\_Imports.razor"
 using BlazorSupervisionRBI.Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "c:\Users\ADOMEON\blazorsupervisionrbi\_Imports.razor"
+#line 12 "c:\Users\ADOMEON\BlazorSupervisionRBI\_Imports.razor"
 using System;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "c:\Users\ADOMEON\blazorsupervisionrbi\_Imports.razor"
+#line 13 "c:\Users\ADOMEON\BlazorSupervisionRBI\_Imports.razor"
 using System.Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "c:\Users\ADOMEON\blazorsupervisionrbi\_Imports.razor"
+#line 14 "c:\Users\ADOMEON\BlazorSupervisionRBI\_Imports.razor"
 using System.Data.SqlClient;
 
 #line default
@@ -118,7 +118,7 @@ using System.Data.SqlClient;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 94 "c:\Users\ADOMEON\blazorsupervisionrbi\Pages\Node.razor"
+#line 94 "c:\Users\ADOMEON\BlazorSupervisionRBI\Pages\Node.razor"
        
     private List<Overview> overviewNode;
     private Dictionary<int, List<DetailsNode>> detailsNodeBySeverity = new Dictionary<int, List<DetailsNode>>();
